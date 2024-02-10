8085eface03e2093c4d18f0f09d5c13f74a20ab1
KateArsenina <katti.alexandr@gmail.com>
2024-02-10 23:05:08 +0200

    Homework task about years - text editions

--- years-task.js
+++ years-task.js
@@ -7,12 +7,12 @@ age = Number(age);
 if (isNaN(age) || age <= 0 || age > 100) {
     console.log(age, "Це не число від 0 до 100");
 } else if (age >= 2 && age <= 4) {
-    console.log("Ваш вік ", + age, "роки");
+    console.log("Ваш вік " + age + " роки");
 } else if (age == 1 || age == 21 || age == 31
     || age == 41 || age == 51 || age == 61
     || age == 71 || age == 81 || age == 91) {
-    console.log("Ваш вік ", + age, "рік");
+    console.log("Ваш вік " + age + " рік");
 }
 else {
-    console.log("Ваш вік ", + age, "років");
+    console.log("Ваш вік " + age + " років");
 }
