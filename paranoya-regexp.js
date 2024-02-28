9d161eb41404a5c672eb1364ee09a8b67bef9ad5
KateArsenina <katti.alexandr@gmail.com>
2024-02-28 17:20:27 +0200

    Homework paranoya - final fixes

--- paranoya-regexp.js
+++ paranoya-regexp.js
@@ -20,21 +20,29 @@ var arr = [
         userName:"Andrii",
         lastName:"",
         email:"andrii@mail.ru" // Нам такі не підходять
+    },
+    {
+        userName:"Olan",
+        lastName:"_",
+        email: "13$*!@2341dmitro.porohov@yahoo.com44$we1Vg54"
     }
 ];
 
 // Функція, яка перевіряє масив та виводить емейли з довіреними доменами
 function checkForTrustedEmails() {
-    let regExp = /\w*.\w@(gmail|yahoo)(.com)|(^.ru)/gm;
-    let trustedEmails = [];
+    let regExp = /^([a-z0-9]+\.?[a-z0-9]+)@(gmail|yahoo)\.com$/;
+    // let trustedEmails = [];
+    let trust = {};
     let obj = {};
     for (let i in arr) {
         obj = arr[i];
-        if (obj.email.match(regExp) !== null) {
-            trustedEmails.push(arr[i]);
-        }
+        let testedEmail = obj.email;
+                if (regExp.test(testedEmail) === true ) {
+                    trust["email-" + i] = obj.email;
+                }
+
     }
-    console.log("Trusted account(s):\n", trustedEmails);
+    console.log("Trusted account(s):\n", trust);
 }
 
 checkForTrustedEmails(arr);
